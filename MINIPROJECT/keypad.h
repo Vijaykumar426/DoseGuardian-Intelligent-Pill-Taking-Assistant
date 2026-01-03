c414165717a5e339ddbe6288bc06a2bc7ccaca56
Jarpla Vijay Kumar <jarplavijaykumar426@gmail.com>
2026-01-03 14:43:39 +0530

    Refactor keypad.h to include new functions and types

--- MINIPROJECT/keypad.h
+++ MINIPROJECT/keypad.h
@@ -1,3 +1,8 @@
-void keypadinit(void);
-int colstatus(void);
-char readkeyval(void);
+/* File Name : keypad.h */
+
+#include"types.h"                    // user-defined data types
+
+void keypadinit(void);               // initialize keypad rows and columns
+s32 colstatus(void);                 // check column status of keypad
+s8 readkeyval(void);                 // read pressed key value
+u32 get_number_from_keypad(void);    // read numeric input from keypad
